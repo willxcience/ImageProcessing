7a4279813b14df90480bbd74ec1289ded8ed9f3b
George Burger <gwburger@ucdavis.edu>
2017-02-20 20:25:09 -0800

    Upload multiple files at once

---
+++ Server.js
@@ -0,0 +1,33 @@
+var express	=	require("express");
+var bodyParser =	require("body-parser");
+var multer	=	require('multer');
+var app	=	express();
+app.use(bodyParser.json());
+var storage	=	multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+var upload = multer({ storage : storage }).array('userPhoto',4);
+
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/photo',function(req,res){
+	upload(req,res,function(err) {
+		console.log(req.body);
+		console.log(req.files);
+		if(err) {
+			return res.end("Error uploading file.");
+		}
+		res.end("File is uploaded");
+	});
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
--- index.js
+++ index.js
@@ -5,9 +5,14 @@
 const path = require('path')  
 const express = require('express')  
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const fs = require('fs')
 
 const app = express()
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var upload = multer({ dest: '/tmp/'});
 
 app.engine('.hbs', exphbs({  
   defaultLayout: 'main',
@@ -16,6 +21,29 @@ app.engine('.hbs', exphbs({
 }))
 app.set('view engine', '.hbs')  
 app.set('views', path.join(__dirname, 'views'))
+
+// File input field name is simply 'file'
+app.post('/file_upload', upload.single("file"), function (req, res) {
+   var file = __dirname + "/" + req.file.originalname;
+   fs.readFile( req.file.path, function (err, data) {
+        fs.writeFile(file, data, function (err) {
+         if( err ){
+              console.error( err );
+              response = {
+                   message: 'Sorry, file couldn\'t be uploaded.',
+                   filename: req.file.originalname
+              };
+         }else{
+               response = {
+                   message: 'File uploaded successfully',
+                   filename: req.file.originalname
+              };
+          }
+          res.end( JSON.stringify( response ) );
+       });
+   });
+})
+
 
 app.post('/', (request, response) => {
     console.log(request)
@@ -27,7 +55,7 @@ app.get('/', (request, response) => {
   response.render('home', {
     name: 'John'
   })
-    console.log(request.foo)
+    //console.log(request.foo)
 })
 
 app.listen(3000) 
