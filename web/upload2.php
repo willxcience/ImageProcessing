abae1898da7bbbac75f7b39c7c5b24ae99a6d6d8
Yishen <Houlor@163.com>
2017-03-12 01:41:11 -0800

    Update upload2.php

--- web/upload2.php
+++ web/upload2.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL); ini_set('display_errors', '1');
+$target_dir = "uploads/";
 $host= "localhost";
 $username="root";
 $userpass="usbw";
@@ -9,12 +11,20 @@ if ($mysqli->connect_errno){
  }
 $count = 0;
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    echo $_FILES['files']['name'][1];
     foreach ($_FILES['files']['name'] as $i => $name) {
         if (strlen($_FILES['files']['name'][$i]) > 1) {
               $fileName = $_FILES['files']['name'][$i];
               $tmpName  = $_FILES['files']['tmp_name'][$i];
               $fileSize = $_FILES['files']['size'][$i];
               $fileType = $_FILES['files']['type'][$i];
+
+	      $target_file = $target_dir . basename($fileName);
+ 	      if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $target_file)) {
+        echo "The SP file ". basename( $_FILES['files']['name'][$i]). " has been uploaded.";
+    }
+else {echo "The SP file failed";}
+
               
               $fp      = fopen($tmpName, 'r');
               $content = fread($fp, filesize($tmpName));
